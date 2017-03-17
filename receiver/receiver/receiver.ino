6edbcb02615c2bf31e9288e06c6489720efdb098
presneto <metod.zupancic@gmail.com>
2017-03-17 09:29:42 +0100

    Add files via upload

---
+++ receiver/receiver/receiver.ino
@@ -0,0 +1,29 @@
+//OBVEZNE KNJIŽNICE IN OBJEKT/////
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7, 8);
+
+//////////////////////////////////
+const byte rxAddr[6] = "00001";
+
+void setup()
+{
+  Serial.begin(9600);//če rabš serijsko komunikacijo
+  
+  radio.begin();//zagon modula
+  radio.openReadingPipe(0, rxAddr);//ker ima 0, je sprejemnik??
+  
+  radio.startListening();//vklop prejemanja podatkov
+}
+
+void loop()
+{
+  if (radio.available())// če smo prejeli podatke =1 -> drugače =0
+  {
+    char text[32] = {0};//če dobimo podatke naredimo char tabelo
+    radio.read(&text, sizeof(text));//za branje prejetih podatkov
+    Serial.println(text);
+  }
+}
---
+++ transmitter/transmitter.ino
@@ -0,0 +1,26 @@
+#include <SPI.h> //za komunikacijski interface z modemom
+#include <nRF24L01.h>// za driver tega radio modema
+#include <RF24.h> // kontroliranje radio modema
+
+RF24 radio(7, 8); //nov objekt knjižnice
+
+const byte rxAddr[6] = "00001"; //globalna tabela rxAddr->naslov modema
+                                //da se loči od ostalih wifi modemov
+void setup()
+{
+  radio.begin();//zagon modema
+  radio.setRetries(15, 15);//število ponovitev v primeru nepopolnega prenosa
+  radio.openWritingPipe(rxAddr);//naslov prejemnika, kamor so pošljeni podatki
+  
+  radio.stopListening();//preklop iz ukaznega načina na podatkovno pošiljanje
+}
+
+void loop()
+{
+  const char text[] = "Hello World";
+  radio.write(&text, sizeof(text));//pošiljanje na modem.
+  //dokler radio.write() ne dobi potrditve ali pa je konec poskusov
+  //se program blokira.
+  
+  delay(1000);
+}
